29f92cbcca6105c7ff247b4f0375044ccbc487bc
Logicx <81425330+YLogicx@users.noreply.github.com>
2025-01-28 11:33:39 +0100

    update 1.1

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ window.addEventListener('load', () => {
     const mainSection = document.querySelector('.main-section');
     mainSection.classList.add('loaded');
   });
+
+  window.addEventListener('load', () => {
+    const mainSection = document.querySelector('.main_title');
+    mainSection.classList.add('loaded');
+  });
